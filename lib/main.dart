92d88c7c2a46494522de9638f687344bc69e7862
akarshbr <akarshbrajeev@gmail.com>
2024-03-20 12:21:34 +0530

    drop down added on signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:placement_app/presentation/common/signup_screen/controller/signup_controller.dart';
 import 'package:placement_app/presentation/common/signup_screen/view/signup_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => SignupController()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
