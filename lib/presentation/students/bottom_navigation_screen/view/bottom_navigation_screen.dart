e1072506b5ffe4bb62893d446c03f5b8a199a014
NITHA <nithaparveenk@gmail.com>
2024-03-25 16:39:19 +0530

    quiz page added/btm nav bar changed

--- lib/presentation/students/bottom_navigation_screen/view/bottom_navigation_screen.dart
+++ lib/presentation/students/bottom_navigation_screen/view/bottom_navigation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:placement_app/core/constants/color_constants.dart';
 import 'package:placement_app/presentation/students/apply_jobs_screen/view/apply_jobs_screen.dart';
 import 'package:placement_app/presentation/students/bottom_navigation_screen/controller/bottom_navigation_controller.dart';
 import 'package:placement_app/presentation/students/home_screen/view/student_home_screen.dart';
@@ -20,19 +21,28 @@ class BottomNavigationScreen extends StatelessWidget {
         }
       ),
       bottomNavigationBar: Consumer<BottomNavigationController>(builder: (context, controller, _) {
-        return BottomNavigationBar(
-            onTap: (index) {
-              controller.currentIndex = index;
-            },
-            currentIndex: controller.currentIndex,
-            type: BottomNavigationBarType.fixed,
-            unselectedItemColor: Colors.white,
-            selectedItemColor: Colors.blueGrey,
-            items: [
-              BottomNavigationBarItem(icon: Icon(Icons.menu_book_sharp), label: "Material"),
-              BottomNavigationBarItem(icon: Icon(Icons.add), label: "Apply"),
-              BottomNavigationBarItem(icon: Icon(Icons.quiz), label: "Quiz")
-            ]);
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            padding: const EdgeInsets.all(5.0),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: ColorConstants.primary),
+            child: BottomNavigationBar(selectedIconTheme:IconThemeData(color: ColorConstants.white),elevation:0,backgroundColor: ColorConstants.primary,
+                onTap: (index) {
+                  controller.currentIndex = index;
+                },
+                currentIndex: controller.currentIndex,
+                type: BottomNavigationBarType.fixed,
+                unselectedItemColor: ColorConstants.grey,
+                selectedItemColor: ColorConstants.white,
+                showSelectedLabels: true,
+                showUnselectedLabels: false,
+                items: [
+                  BottomNavigationBarItem(icon: Icon(Icons.menu_book_sharp), label: "Material"),
+                  BottomNavigationBarItem(icon: Icon(Icons.add), label: "Apply"),
+                  BottomNavigationBarItem(icon: Icon(Icons.quiz), label: "Quiz")
+                ]),
+          ),
+        );
       }),
     );
   }
