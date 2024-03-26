5c005f05bb15329fce2a1b37577aa714ff9227e3
NITHA <nithaparveenk@gmail.com>
2024-03-26 15:47:12 +0530

    student_company screen added

--- lib/presentation/common/student_company/student_company.dart
+++ lib/presentation/common/student_company/student_company.dart
@@ -8,12 +8,11 @@ import 'package:placement_app/presentation/common/login_screen/view/login_screen
 import 'package:placement_app/presentation/common/registration/company/view/company_reg_scrn.dart';
 import 'package:placement_app/presentation/common/registration/student/view/student_register_scrn.dart';
 import 'package:placement_app/presentation/common/registration/tpo/view/tpo_reg_scrn.dart';
-import 'package:placement_app/presentation/common/student_company/student_company.dart';
 
 
 
-class GetStarted extends StatelessWidget {
-  const GetStarted({super.key});
+class StudentCompany extends StatelessWidget {
+  const StudentCompany({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -24,74 +23,43 @@ class GetStarted extends StatelessWidget {
             backgroundColor: const Color(0xFFeef1f3),
             appBar: AppBar(
               backgroundColor: const Color(0xFFeef1f3),
-              actions: [
-                PopupMenuButton(
-                    iconSize: 20,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    icon: Icon(Icons.more_vert),
-                    itemBuilder: (context) {
-                      return [
-                        PopupMenuItem(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => TpoRegistration(),
-                                ));
-                          },
-                          child: Text(
-                            "Training & Placement Officer",
-                            style: GLTextStyles.labeltxtBlk16,
-                          ),
-                        ),
-                      ];
-                    })
-              ],
             ),
             body: Container(
               padding: EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    "Get Registered Here",
-                    style: GoogleFonts.poppins(
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.w700,
-                      color: ColorTheme.black,
-                    ),
-                  ),
+                  // Text(
+                  //   "Get Registered Here",
+                  //   style: GoogleFonts.poppins(
+                  //     fontSize: 26.0,
+                  //     fontWeight: FontWeight.w700,
+                  //     color: ColorTheme.black,
+                  //   ),
+                  // ),
                   LottieBuilder.asset("asset/animation/getStarted.json"),
                   SizedBox(
                     height: 10,
                   ),
                   GLMetrialButton(
-                    text: "Login Here",
+                    text: "Login as Student",
                     color: ColorTheme.darkClr,
                     txtClr: ColorTheme.white,
                     height: width * .15,
-                    route: StudentCompany(),
+                    route: LoginScreen(),
                   ),
                   SizedBox(
                     height: 10,
                   ),
                   GLMetrialButton(
-                    text: "Student",
+                    text: "Login as Company",
                     color: ColorTheme.darkClr,
                     txtClr: ColorTheme.white,
                     height: width * .15,
-                    route: StudentRegister(),
+                    route: LoginScreen(),
                   ),
                   SizedBox(
                     height: 10,
-                  ),
-                  GLMetrialButton(
-                    text: "Company",
-                    color: ColorTheme.darkClr,
-                    txtClr: ColorTheme.white,
-                    height: width * .15,
-                    route: CompanyRegistration(),
                   ),
                 ],
               ),
