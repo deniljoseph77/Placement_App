06a36e2b71eca0fc16b7aaf9a5feb04b220ac5ba
akarshbr <akarshbrajeev@gmail.com>
2024-03-26 23:09:23 +0530

    app config modified

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -3,4 +3,8 @@ class AppConfig {
   static String baseurl = "http://192.168.29.213:8000/";
   //mediaurl
   static String mediaurl = "http://192.168.29.213:8000";
+
+  static const String loginDataKey = "loginData";
+
+  static const String token = "token";
 }
