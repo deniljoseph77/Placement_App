e7e0277ca2c84b059f7068495cb2807a8618b4d7
akarshbr <akarshbrajeev@gmail.com>
2024-03-27 22:30:03 +0530

    post job partially completed

--- lib/presentation/company/bottom_navigation_screen/controller/bottom_navigation_controller.dart
+++ lib/presentation/company/bottom_navigation_screen/controller/bottom_navigation_controller.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:placement_app/presentation/company/home_screen/view/company_homescreen.dart';
-import 'package:placement_app/presentation/company/postjob_screen/view/postjob_screen.dart';
+import 'package:placement_app/presentation/company/postjob_screen/view/post_job_screen.dart';
 
 import '../../jobs_posted/view/jobs_posted_screen.dart';
 
