348439b895c4c63b184f7c5f68a23599c6f2066c
NITHA <nithaparveenk@gmail.com>
2024-04-01 15:12:53 +0530

    minor details 4

--- lib/presentation/company/jobs_posted/view/jobs_posted_screen.dart
+++ lib/presentation/company/jobs_posted/view/jobs_posted_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:placement_app/presentation/company/jobs_posted/controller/job_posted_controller.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../core/constants/color_constants.dart';
 import '../../postjob_screen/view/widgets/posted_jobs_details_card.dart';
 
 class JobsPostedScreen extends StatefulWidget {
@@ -38,24 +39,37 @@ class _JobsPostedScreenState extends State<JobsPostedScreen> {
               Consumer<JobsPostedController>(builder: (context, controller, _) {
             return controller.isLoading
                 ? Center(child: CircularProgressIndicator())
-                : ListView.builder(
-                    itemCount: controller.postedJobsModel.data?.length,
-                    itemBuilder: (context, index) {
-                      return PostedJobsDetailsCard(
-                        size: size,
-                        companyName:
-                            controller.postedJobsModel.data?[index].postedBy,
-                        jobTitle:
-                            controller.postedJobsModel.data?[index].position,
-                        description:
-                            controller.postedJobsModel.data?[index].description,
-                        requirement: controller
-                            .postedJobsModel.data?[index].requirements,
-                        salary: controller.postedJobsModel.data?[index].salary,
-                        lastDate:
-                            controller.postedJobsModel.data?[index].deadline.toString(),
-                      );
-                    });
+                : controller.postedJobsModel.data == null ||
+                        controller.postedJobsModel.data!.isEmpty
+                    ? Center(
+                        child: Text(
+                          "No Data Found",
+                          style: TextStyle(
+                              color: ColorTheme.red,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    : ListView.builder(
+                        itemCount: controller.postedJobsModel.data?.length,
+                        itemBuilder: (context, index) {
+                          return PostedJobsDetailsCard(
+                            size: size,
+                            companyName: controller
+                                .postedJobsModel.data?[index].postedBy,
+                            jobTitle: controller
+                                .postedJobsModel.data?[index].position,
+                            description: controller
+                                .postedJobsModel.data?[index].description,
+                            requirement: controller
+                                .postedJobsModel.data?[index].requirements,
+                            salary:
+                                controller.postedJobsModel.data?[index].salary,
+                            lastDate: controller
+                                .postedJobsModel.data?[index].deadline
+                                .toString(),
+                          );
+                        });
           }),
         ),
       ),
